71844ff01d25f2d37e17bfad7e2ab0739d2bee2e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-15 16:34:02 +0000

    Deploy akhila2019/FlexionReactNewProject to github.com/akhila2019/FlexionReactNewProject.git:gh-pages

---
+++ precache-manifest.4f9142e747e782d83c12a3cfa91dc9fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "647d3993691d5b0d75f9",
+    "url": "/FlexionReactNewProject/static/js/runtime~main.18973210.js"
+  },
+  {
+    "revision": "4c2356a3bd8a45848a0d",
+    "url": "/FlexionReactNewProject/static/js/main.1581d504.chunk.js"
+  },
+  {
+    "revision": "3923875cbe9f886d361e",
+    "url": "/FlexionReactNewProject/static/js/2.907c8157.chunk.js"
+  },
+  {
+    "revision": "4c2356a3bd8a45848a0d",
+    "url": "/FlexionReactNewProject/static/css/main.c1127f42.chunk.css"
+  },
+  {
+    "revision": "3923875cbe9f886d361e",
+    "url": "/FlexionReactNewProject/static/css/2.71563047.chunk.css"
+  },
+  {
+    "revision": "566e7f7b03ab9a6feecb86145ad2542b",
+    "url": "/FlexionReactNewProject/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/FlexionReactNewProject/precache-manifest.4f9142e747e782d83c12a3cfa91dc9fc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/FlexionReactNewProject/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
